5c8473c8a0949cb66ce6a6a2e7e8fe36ce758a1c
Kenny Mei <kennymeihd@gmail.com>
2020-10-23 19:32:33 -0400

    Part I - Added ability for user to see app icon in home screen and styled launch screen, for user to sign up to create an account, for user to log in, for user to take a photo, add a caption, and post it to the server, and for user to view the last 20 posts.

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "ubcBXHvcGUbzEUop2Co976xZplqmw0m4Cer6DzVT"
+            $0.clientKey = "jlUAFz6D05xLMScOO0rm88nzFzI5l2VV74iexr6V"
+            $0.server = "https://parseapi.back4app.com/"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
